d6c716ba5fade0356f0f8c1b6479235894cac16b
VinuPolly-Bonnet <90182632+VinuPolly-Bonnet@users.noreply.github.com>
2024-04-20 20:48:04 +0100

    Fixing the preview bug in generated code from kmp website - commonMain.App.kt cannot import import org.jetbrains.compose.ui.tooling.preview.Preview, so have to do in each platform separately.
    iOS doesn't have preview

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,9 +1,16 @@
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.desktop.ui.tooling.preview.Preview
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication, title = "My Bird App") {
         App()
     }
+}
+
+@Preview
+@Composable
+fun AppDesktopPreview() {
+    App()
 }
