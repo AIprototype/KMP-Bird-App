4c8ecedebb628ae2c5d0af3be3f5a7f9a81f6a7f
VinuPolly-Bonnet <90182632+VinuPolly-Bonnet@users.noreply.github.com>
2024-04-25 00:26:46 +0100

    Made the UI have click listener that remembers through recompositon

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,18 +1,7 @@
-import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -21,19 +10,15 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import dev.icerock.moko.mvvm.compose.getViewModel
 import dev.icerock.moko.mvvm.compose.viewModelFactory
 import io.kamel.image.KamelImage
@@ -82,6 +67,7 @@ private fun BirdsPage(
                     )
                 }
             }
+
             is BirdUiState.Success -> {
 
                 /**
@@ -156,28 +142,45 @@ private fun BirdsPage(
 
                         Spacer(modifier = Modifier.height(8.dp))
 
+                        /**
+                         * To make sure the isClicked UI state stays with the item after recomposition and filtering
+                         */
+                        val listStatePair = remember {
+                            it.list.map { birdImage ->
+                                birdImage to mutableStateOf(false)
+                            }
+                        }
+
                         //Content
-                        AnimatedVisibility(it.list.isNotEmpty()) {
+                        AnimatedVisibility(listStatePair.isNotEmpty()) {
                             LazyVerticalGrid(
                                 columns = GridCells.Adaptive(180.dp),
                                 horizontalArrangement = Arrangement.spacedBy(5.dp),
                                 verticalArrangement = Arrangement.spacedBy(5.dp),
                                 modifier = Modifier.fillMaxSize().padding(horizontal = 5.dp)
                             ) {
-                                items(items = it.list.filter { birdImage ->
-                                    (birdImage.category ?: "Unknown")
-                                        .contains(
-                                            other = currentSelectedCat,
-                                            ignoreCase = true
-                                        )
-                                }) { birdImage: BirdImage ->
-                                    BirdImageCell(birdImage)
+
+                                items(
+                                    items = listStatePair
+                                        .filter { birdImagePair ->
+                                            (birdImagePair.first.category ?: "Unknown")
+                                                .contains(
+                                                    other = currentSelectedCat,
+                                                    ignoreCase = true
+                                                )
+                                        }
+                                ) { birdImagePair ->
+                                    BirdImageCell(
+                                        birdImage = birdImagePair.first,
+                                        isClicked = birdImagePair.second
+                                    )
                                 }
                             }
                         }
                     }
                 }
             }
+
             is BirdUiState.Error -> {
                 Box(
                     modifier = Modifier
@@ -197,14 +200,52 @@ private fun BirdsPage(
 
 @Composable
 private fun BirdImageCell(
-    birdImage: BirdImage
+    birdImage: BirdImage,
+    isClicked: MutableState<Boolean>
 ) {
-    KamelImage(
-        resource = asyncPainterResource(data = "https://sebastianaigner.github.io/demo-image-api/${birdImage.imagePath}"),
-        contentDescription = "${birdImage.category} by ${birdImage.author}",
-        contentScale = ContentScale.Crop,
-        modifier = Modifier
-            .fillMaxWidth()
-            .aspectRatio(1.0F)
-    )
+    Box {
+        KamelImage(
+            resource = asyncPainterResource(data = "https://sebastianaigner.github.io/demo-image-api/${birdImage.imagePath}"),
+            contentDescription = "${birdImage.category} by ${birdImage.author}",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(1.0F)
+                .clickable { isClicked.value = !isClicked.value }
+        )
+        AnimatedVisibility(
+            modifier = Modifier
+                .matchParentSize(),
+            enter = fadeIn(),
+            exit = fadeOut(),
+            visible = isClicked.value
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Black,
+                                Color.Transparent,
+                                Color.Black
+                            )
+                        )
+                    )
+            ) {
+                Text(
+                    modifier = Modifier.align(Alignment.TopEnd).padding(8.dp),
+                    text = birdImage.category ?: "Unknown",
+                    color = Color.White,
+                    fontSize = 12.sp
+                )
+                Text(
+                    modifier = Modifier.align(Alignment.BottomStart).padding(8.dp),
+                    text = "by ${birdImage.author ?: "Unknown"}",
+                    color = Color.White,
+                    fontSize = 12.sp
+                )
+            }
+        }
+    }
 }
